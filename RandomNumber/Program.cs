a60ef1aa3b495bbd391134962ef2ed5acb472b80
Imre Kli. <imre.koli@tktk.ee>
2023-11-05 17:34:15 +0200

    add week 4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new Random();
+
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    int myRandomNum = rnd.Next(0, 10); //0 - 10
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My Random number is: {myRandomNum}");
+}
+
+
+Console.WriteLine($"random sum total: {randomSum}");
+
+
